61f8bdad9a9fa0174b8d2eb801a35d85606ad9b9
Alumno <Alumno@PC240>
2025-04-04 14:28:34 -0300

    Agregar archivos de proyecto.

---
+++ clsConexionBD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace pryApellidoConexionBD
+{
+    internal class clsConexionBD
+    {
+        static void Main()
+        {
+            string connectionString = "Server=localhost;Database=Producto;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    ______________("✅ Conexión exitosa a la base de datos.");
+                }
+                catch (Exception ex)
+                {
+                    ______________("❌ Error al conectar: " + ex.Message);
+                }
+            }
+        }
+    }
+
+}
+
